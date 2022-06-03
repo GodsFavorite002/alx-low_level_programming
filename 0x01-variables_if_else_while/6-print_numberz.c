bd375e75aa0f3853d01a8718d1c6ad4c1fd15c28
Favour Wright <fav2wright@gmail.com>
2022-06-03 05:42:20 -0700

    another commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int r;
+
+	for (r = '0'; r <= '9'; r++)
+		putchar(r);
+	putchar('\n');
+
+	return (0);
+}
