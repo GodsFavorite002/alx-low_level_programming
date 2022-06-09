e8be7cb54d579ce33e18737e9e4ef2700956d2a1
Favour Wright <fav2wright@gmail.com>
2022-06-09 10:49:12 -0700

    zero to hero

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int fav;
+
+	for (fav = 48; fav < 58; fav++)
+	{
+		_putchar(fav);
+	}
+	_putchar('\n');
+}
