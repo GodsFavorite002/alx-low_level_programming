c471cdb35ce4734eb6d94bf3f4142c7fff2719ed
Favour Wright <fav2wright@gmail.com>
2022-07-01 08:56:44 -0700

    more memory allocations

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,46 +3,50 @@
 #include <stdlib.h>
 
 /**
- *string_nconcat - concat 2 string
- *@s1: string 1
- *@s2: string 2
- *@n: integer value
- *Return: pointer points to a new allocated space else NULL
+ * string_nconcat - function that concatenates two strings
+ *
+ * @s1: string of chars
+ * @s2: string of chars
+ * @n: size of s2 to copy
+ *
+ * Return: address of the newly allocated memory
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int s1_len, s2_len, i;
+	unsigned int len1, len2 = 0;
+	unsigned int i, j;
+	char *str_copy;
+	char *tmp1 = s1;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	i = 0;
+	while (*s1++)
+		i++;
+	len1 = i;
+	s1 = tmp1;
 
-for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-;
+	len2 = n;
 
-for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-;
+	str_copy = malloc((len1 + len2) * sizeof(char) + 1);
+	if (str_copy == NULL)
+		return (NULL);
 
-str = malloc(s1_len + s2_len + 1);
-
-
-if (n >= s2_len)
-n = s2_len;
-
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-
-for (i = 0; i < n; i++)
-str[s1_len + i] = s2[i];
-
-str[s1_len + n] = '\0';
-
-if (str == NULL)
-return (NULL);
-
-return (str);
+	j = 0;
+	while (j < len1)
+	{
+		str_copy[j] = s1[j];
+		j++;
+	}
+	while (j < len1 + len2)
+	{
+		str_copy[j] = s2[j - len1];
+		j++;
+	}
+	str_copy[j] = '\0';
+	return (str_copy);
 }
